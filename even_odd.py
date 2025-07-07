b1d3bab8b1b90d99c801ff71d8a7df6f66a2d3ab
Jobayer Islam Rafid <jobayer.islam01june@gmail.com>
2025-07-08 03:32:08 +0600

    Add files via upload

---
+++ even_odd.py
@@ -0,0 +1,5 @@
+num = int(input("Enter Number: "))
+if num %2 == 0:
+    print( f"Number {num} Is Even")
+else:
+     print( f"Number {num} Is Odd")   
---
+++ sum_even_numbers.py
@@ -0,0 +1,6 @@
+result = 0
+
+for num in range(2, 101, 2):
+    result += num
+
+print(f"Sum of Numbers : {result}") 
