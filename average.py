63d5f56dfdb26c9fc8d347356ad324c2e4b6322a
Jobayer Islam Rafid <jobayer.islam01june@gmail.com>
2025-07-11 02:01:03 +0600

    Delete average.py

--- average.py
+++
@@ -1,8 +0,0 @@
-num1 = int(input("Enter 1st Number: "))
-num2 = int(input("Enter 2nd Number: "))
-
-def average(num1, num2):
-    return (num1 + num2) / 2
-
-avg = average(num1, num2)
-print(f"The average of {num1} & {num2} is: {avg}")
